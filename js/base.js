6bb0439d2460482d58866feda5b91a00cfab8ef0
Allan Lima <allan.lima@ilhasoft.com.br>
2019-03-30 15:04:25 -0300

    Add chat popup on user avatar click

---
+++ js/base.js
@@ -0,0 +1,15 @@
+function showChatPopup(id) {
+	const popup = document.getElementById(id);
+	popup.classList.toggle('show');
+
+	const items = document.getElementsByClassName('item');
+	const itemsKeys = Object.keys(items);
+
+	itemsKeys.forEach(itemKey => {
+		const itemUserId = items[itemKey].childNodes['1'].childNodes['1'].childNodes['3'].id;
+		if (itemUserId != id) {
+			const popup = document.getElementById(itemUserId);
+			popup.classList.remove('show');
+		}
+	});
+}
